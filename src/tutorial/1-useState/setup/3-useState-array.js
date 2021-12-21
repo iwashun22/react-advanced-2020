11adb5e954925ec321068c3fde79f41a758691fd
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-21 16:24:51 +0700

    update

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,10 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState([...data]);
+  const removeItem = id => {
+    const newPeople = people.filter(person => person.id !== id)
+    setPeople(newPeople);
+  }
   return (
     <React.Fragment>
     {
@@ -12,10 +16,14 @@ const UseStateArray = () => {
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove this item</button>
           </div>
         );
       })
     }
+    <button className="btn" onClick={() => setPeople([])}>
+      clear items
+    </button>
     </React.Fragment>
   );
 };
