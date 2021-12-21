ae6df43ec0f93107afe789a90c49e3b101d273b2
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-21 17:13:36 +0700

    update

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const reset = () => {
+    setValue(0);
+  }
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1); this will not work if you click multiple time
+      setValue(prevState => {
+        return prevState + 1;
+      })
+    }, 2000)
+  }
+  return (
+    <React.Fragment>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+        <button className="btn" onClick={reset}>
+          reset
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>increase delay</button>
+      </section>
+    </React.Fragment>
+  );
 };
 
 export default UseStateCounter;
