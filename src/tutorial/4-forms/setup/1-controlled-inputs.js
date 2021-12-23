19e3d4156f62b1b7334b225afb0381c1f22d851e
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-23 17:38:31 +0700

    update

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,46 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.info(firstName, email);
+  }
+
+  // when you are working on form in React, you need to use onChange to trigger the event and get the input from users, by accessng event.target.value
+  return (
+    <React.Fragment>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input 
+              type="text" 
+              placeholder="your name"
+              name="firstName" 
+              id="firstName" 
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input 
+              type="email" 
+              placeholder="email address"
+              name="email" 
+              id="email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+      </article>
+    </React.Fragment>
+  );
 };
 
 export default ControlledInputs;
