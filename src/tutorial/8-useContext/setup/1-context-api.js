72f6554281090fe52b678e46cdae2e03a9a21cf6
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-28 20:00:34 +0700

    update

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,9 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// two components: Provider, Consumer
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,14 +14,15 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
+    <PersonContext.Provider value={{ message:'hello', removePerson, people }}>
       <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const { people } = useContext(PersonContext);
   return (
     <>
       {people.map((person) => {
@@ -26,7 +30,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +37,9 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson, message } = useContext(PersonContext);
+  console.log(message);
   return (
     <div className='item'>
       <h4>{name}</h4>
